69eb6d47cb4cd0a69a9d5db8c869bf25e07a66b0
Piotr Janus <pjanus@secureauth.com>
2025-03-31 12:18:58 +0200

    add loggin with iframe and split logic to different routes using react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,43 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { UserManager, UserManagerSettings, User } from 'oidc-client-ts';
+import { User } from 'oidc-client-ts';
+import {
+  Route,
+  Routes,
+  Link,
+  useNavigate
+} from "react-router-dom";
+import { Iframe, IframeCallback, IframeLogin } from './Iframe';
+import { PopupCallback } from './Popup';
+import { LogoutCallback } from './Logout';
+import { client } from './oidc-settings';
 
 function App() {
-
-
-  const settings: UserManagerSettings = {
-    authority: 'https://janus.eu.authz.cloudentity.io/janus/demo',
-    client_id: '5db70ee5e0444f8f88b7acd396a685cc',
-    redirect_uri: 'https://localhost:3000/callback',
-    response_type: 'code',
-    scope: 'openid profile',
-    automaticSilentRenew: true,
-    response_mode: 'fragment',
-    popup_redirect_uri: 'https://localhost:3000/callback',
-    popup_post_logout_redirect_uri: 'https://localhost:3000/logoutCallback',
-    silent_redirect_uri: 'https://localhost:3000/silentCallback',
-    filterProtocolClaims: true
-};
-
-  const client = new UserManager(settings);
-  const optionalArgs = { 
-    state: { bar: 15 } 
-  };
+  const navigate = useNavigate();
 
   const [user, setUser] = useState<User | null>(null);
   useEffect(() => {
-    client.signinPopupCallback().then(function() {
-      console.log("signin popup callback response success");
+    window.addEventListener('message', function(event) {
+      if (event.data.type && event.data.type === "oidc.user") {
+        console.log("message received", event);
+        client.getUser().then(function(user) {
+          console.log("user", user);
+          setUser(user);
+          navigate("/");
+        });
+      }
+    });
+    client.getUser().then(function(user) {
+      console.log("user", user);
+      setUser(user);
     }).catch(function(err) {
       console.error(err);
     });
   }, []);
 
   const login = () => {
-    client.signinPopup(optionalArgs).then(function(user) {
+    client.signinPopup().then(function(user) {
       console.log("signed in", user);
       setUser(user);
     }).catch(function(err) {
@@ -55,18 +57,36 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        { !user && <button onClick={login}>Login</button> }
-        { user && <div>
-          <p>Logged in as {user.profile.name}</p>
-          <button onClick={logout}>Logout</button>
-        </div> }
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          { !user && <div>
+            <button onClick={login}>Login</button>
+            <Link to="../iframe"><button>Login iframe</button></Link>
+          </div> }
+          <Routes>
+            <Route path="/logoutCallback" element={<LogoutCallback />} />
+            <Route path="/popupCallback" element={<PopupCallback />} />
+            <Route path="/iframeCallback" element={<IframeCallback />} />
+            <Route path="/iframeLogin" element={<IframeLogin />}/>
+            <Route path="/iframe" element={<Iframe />}/>
+            <Route path="/" element={ user && <LoggedIn user={user} logout={logout} />} />
+          </Routes>
+        </header>
+      </div>
+  );
+}
+
+interface LoggedInProps {
+  user: User;
+  logout: () => void;
+}
+
+const LoggedIn = (props: LoggedInProps) => {
+  return (
+    <div>
+      <p>Logged in as {props.user.profile?.name}</p>
+      <button onClick={props.logout}>Logout</button>
     </div>
   );
 }
